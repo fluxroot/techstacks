bda4b2ae9dc994b07e278bda6c701e797b445880
Phokham Nonava <pn@nonava.com>
2021-04-03 23:08:55 +0200

    Add Quarkus project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "techstacks"
+
+val projects = listOf(
+    "quarkus"
+).forEach { configureProject(it) }
+
+fun configureProject(projectName: String) {
+    include(projectName)
+    val project = project(":${projectName}")
+    project.buildFileName = "${projectName}.gradle.kts"
+    project.projectDir = rootDir.resolve(projectName)
+}
