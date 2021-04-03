9ef17b838c713ed789ef92907998ecc5ee1d5859
Phokham Nonava <pn@nonava.com>
2021-04-04 00:31:08 +0200

    Add Greeting domain model

---
+++ quarkus/src/main/java/dev/nonava/techstacks/quarkus/greeting/domain/model/Greeting.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2021 Phokham Nonava
+ *
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package dev.nonava.techstacks.quarkus.greeting.domain.model;
+
+import dev.nonava.techstacks.quarkus.core.domain.model.Entity;
+import dev.nonava.techstacks.quarkus.core.domain.model.Identity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Greeting implements Entity<Greeting> {
+    private final Identity<Greeting> id;
+    private final Username username;
+
+    public Greeting(Username username) {
+        this.id = Identity.of(Greeting.class);
+        this.username = username;
+    }
+}
---
+++ quarkus/src/main/java/dev/nonava/techstacks/quarkus/greeting/domain/model/Username.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021 Phokham Nonava
+ *
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package dev.nonava.techstacks.quarkus.greeting.domain.model;
+
+import lombok.Value;
+
+@Value
+public class Username {
+    String value;
+}
