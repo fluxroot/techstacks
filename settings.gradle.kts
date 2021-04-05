18c02a8cbfc6660ab4b2d9a0760364c26c9a9529
Phokham Nonava <pn@nonava.com>
2021-04-05 14:27:09 +0200

    Remove customization code from settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
 rootProject.name = "techstacks"
 
-val projects = listOf(
-    "quarkus"
-).forEach { configureProject(it) }
-
-fun configureProject(projectName: String) {
-    include(projectName)
-    val project = project(":${projectName}")
-    project.buildFileName = "${projectName}.gradle.kts"
-    project.projectDir = rootDir.resolve(projectName)
-}
+include("quarkus")
