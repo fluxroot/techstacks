e317da455d16a3dd67a4f494ac368c293b09919f
Phokham Nonava <pn@nonava.com>
2021-04-05 14:24:27 +0200

    Remove archive optimizations

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,4 @@ allprojects {
 
     group = "dev.nonava.techstacks"
     version = "1.0.0"
-
-    tasks.withType<AbstractArchiveTask> {
-        isPreserveFileTimestamps = false
-        isReproducibleFileOrder = true
-    }
 }
