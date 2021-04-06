c1ed67debbf9e9429851f8f6bad9169b9200aacd
Phokham Nonava <pn@nonava.com>
2021-04-06 23:12:43 +0200

    Improve Result class

--- spring/src/main/java/dev/nonava/techstacks/spring/core/domain/model/Result.java
+++ spring/src/main/java/dev/nonava/techstacks/spring/core/domain/model/Result.java
@@ -9,124 +9,91 @@ package dev.nonava.techstacks.spring.core.domain.model;
 
 import lombok.EqualsAndHashCode;
 
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 @EqualsAndHashCode
-public final class Result<R, F> {
-    private final R result;
+public final class Result<S, F> {
+    private final S success;
     private final F failure;
 
-    private Result(R result, F failure) {
-        requireEitherResultOrFailure(result, failure);
-        this.result = result;
+    private Result(S success, F failure) {
+        requireEitherSuccessOrFailure(success, failure);
+        this.success = success;
         this.failure = failure;
     }
 
-    public static <R, F> Result<R, F> of(R result) {
-        return new Result<>(result, null);
+    public static <S, F> Result<S, F> of(S success) {
+        return new Result<>(success, null);
     }
 
-    public static <R, F> Result<R, F> failure(F failure) {
+    public static <S, F> Result<S, F> failure(F failure) {
         return new Result<>(null, failure);
     }
 
-    public boolean isPresent() {
-        return result != null;
+    public boolean isSuccess() {
+        return success != null;
     }
 
     public boolean isFailure() {
         return failure != null;
     }
 
-    public boolean contains(R expectedResult) {
-        return isPresent() && Objects.equals(result, expectedResult);
-    }
-
-    public boolean containsFailure(F expectedFailure) {
-        return isFailure() && Objects.equals(failure, expectedFailure);
-    }
-
-    public void ifPresent(Consumer<? super R> action) {
-        if (isPresent()) {
-            action.accept(result);
+    public void onSuccess(Consumer<? super S> action) {
+        if (isSuccess()) {
+            action.accept(success);
         }
     }
 
-    public void ifFailure(Consumer<? super F> action) {
+    public void onFailure(Consumer<? super F> action) {
         if (isFailure()) {
             action.accept(failure);
         }
     }
 
-    public <E extends Throwable> R orElseThrow(Function<F, E> failureMapper) throws E {
+    public <E extends Throwable> S orElseThrow(Function<F, E> onFailure) throws E {
         if (isFailure()) {
-            throw failureMapper.apply(failure);
+            throw onFailure.apply(failure);
         }
-        return result;
+        return success;
     }
 
-    public R get() {
-        if (isFailure()) {
-            throw new NoSuchElementException("No result present");
-        }
-        return result;
-    }
-
-    public F getFailure() {
-        if (isPresent()) {
-            throw new NoSuchElementException("No failure present");
-        }
-        return failure;
-    }
-
-    public Optional<R> peek() {
-        return Optional.ofNullable(result);
-    }
-
-    public Optional<F> peekFailure() {
-        return Optional.ofNullable(failure);
-    }
-
-    public <S> Result<S, F> map(Function<? super R, ? extends S> mapper) {
-        if (isPresent()) {
-            return Result.of(mapper.apply(result));
+    public <U> Result<U, F> map(Function<? super S, ? extends U> onSuccess) {
+        if (isSuccess()) {
+            return Result.of(onSuccess.apply(success));
         } else {
             return Result.failure(failure);
         }
     }
 
-    public <S> Result<S, F> flatMap(Function<? super R, ? extends Result<S, F>> mapper) {
-        if (isPresent()) {
-            return mapper.apply(result);
+    public <U> Result<U, F> flatMap(Function<? super S, ? extends Result<U, F>> onSuccess) {
+        if (isSuccess()) {
+            return onSuccess.apply(success);
         } else {
             return Result.failure(failure);
         }
     }
 
-    public <S> S transform(Function<? super R, S> resultMapper, Function<? super F, S> failureMapper) {
-        if (isPresent()) {
-            return resultMapper.apply(result);
+    public <U> U transform(Function<? super S, U> onSuccess, Function<? super F, U> onFailure) {
+        if (isSuccess()) {
+            return onSuccess.apply(success);
         } else {
-            return failureMapper.apply(failure);
+            return onFailure.apply(failure);
         }
     }
 
     @Override
     public String toString() {
-        if (isPresent()) {
-            return String.format("Result[%s]", result);
+        if (isSuccess()) {
+            return String.format("Success[%s]", success);
         } else {
             return String.format("Failure[%s]", failure);
         }
     }
 
-    private static <R, F> void requireEitherResultOrFailure(R result, F failure) {
-        if ((result == null) == (failure == null)) {
-            throw new IllegalArgumentException("Either result or failure must be set");
+    private static <S, F> void requireEitherSuccessOrFailure(S success, F failure) {
+        if ((success == null) == (failure == null)) {
+            throw new IllegalArgumentException("Either success or failure must be set");
         }
     }
 }
